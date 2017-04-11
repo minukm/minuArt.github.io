7b79954e73916f07d91b6ba0d84780d1ad4de2f2
minu kumari <minkumari@deloitte.com>
2017-04-11 10:55:20 +0530

    go to top

--- functions.js
+++ functions.js
@@ -37,3 +37,16 @@ function onScroll(event){
         }
     });
 }
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
